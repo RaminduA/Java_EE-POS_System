06369b57073eeef96aecb697951a62176e2b3299
RaminduA <raminduanjana22171@gmail.com>
2022-06-17 10:00:09 +0530

    Updated OrderDetail entity

--- Backend/src/entity/OrderDetail.java
+++ Backend/src/entity/OrderDetail.java
@@ -3,17 +3,17 @@ package entity;
 public class OrderDetail {
     private String orderId;
     private String itemCode;
+    private double unitPrice;
     private int orderQty;
-    private double discount;
     private double price;
 
     public OrderDetail() {  }
 
-    public OrderDetail(String orderId, String itemCode, int orderQty, double discount, double price) {
+    public OrderDetail(String orderId, String itemCode, double unitPrice, int orderQty, double price) {
         this.setItemCode(itemCode);
         this.setOrderId(orderId);
+        this.setUnitPrice(unitPrice);
         this.setOrderQty(orderQty);
-        this.setDiscount(discount);
         this.setPrice(price);
     }
     public String getOrderId() {
@@ -30,18 +30,18 @@ public class OrderDetail {
         this.itemCode = itemCode;
     }
 
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
     public int getOrderQty() {
         return orderQty;
     }
     public void setOrderQty(int orderQty) {
         this.orderQty = orderQty;
-    }
-
-    public double getDiscount() {
-        return discount;
-    }
-    public void setDiscount(double discount) {
-        this.discount = discount;
     }
 
     public double getPrice() {
@@ -56,8 +56,8 @@ public class OrderDetail {
         return "OrderDetail{" +
                 "orderId='" + orderId + '\'' +
                 ", itemCode='" + itemCode + '\'' +
+                ", unitPrice=" + unitPrice +
                 ", orderQty=" + orderQty +
-                ", discount=" + discount +
                 ", price=" + price +
                 '}';
     }
