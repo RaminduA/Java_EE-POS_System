f652de45aa9e3dc405b579b5b9e7bdd5fd8f071c
RaminduA <raminduanjana22171@gmail.com>
2022-06-18 22:48:26 +0530

    Updated ItemBo

--- Backend/src/bo/custom/ItemBO.java
+++ Backend/src/bo/custom/ItemBO.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.util.ArrayList;
 
 public interface ItemBO extends SuperBO {
+    ItemDTO getItem(Connection con, String code);
     boolean addItem(Connection con, ItemDTO dto);
     boolean updateItem(Connection con, ItemDTO dto);
     boolean deleteItem(Connection con, String code);
