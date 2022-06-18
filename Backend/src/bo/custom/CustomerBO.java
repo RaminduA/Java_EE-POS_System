932869e718dfe78f161cc168890d0c8d76984751
RaminduA <raminduanjana22171@gmail.com>
2022-06-18 22:35:41 +0530

    Updated CustomerBo

--- Backend/src/bo/custom/CustomerBO.java
+++ Backend/src/bo/custom/CustomerBO.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.util.ArrayList;
 
 public interface CustomerBO extends SuperBO {
+    CustomerDTO getCustomer(Connection con, String id);
     boolean addCustomer(Connection con, CustomerDTO dto);
     boolean updateCustomer(Connection con, CustomerDTO dto);
     boolean deleteCustomer(Connection con, String id);
